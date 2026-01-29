391263680e0aee81b30508daee9932e715267d28
martin cene <cenemartin14@gmail.com>
2026-01-29 23:12:01 +0100

    Add frontend pages

---
+++ frontend/flights.data.js
@@ -0,0 +1,26 @@
+const flights = [
+  {
+    id: 1,
+    number: "AL101",
+    from: "Tirana (TIA)",
+    to: "Rome (FCO)",
+    time: "08:30 AM",
+    seats: 12,
+  },
+  {
+    id: 2,
+    number: "AL202",
+    from: "Tirana (TIA)",
+    to: "Vienna (VIE)",
+    time: "11:15 AM",
+    seats: 5,
+  },
+  {
+    id: 3,
+    number: "AL303",
+    from: "Tirana (TIA)",
+    to: "Istanbul (IST)",
+    time: "03:45 PM",
+    seats: 20,
+  },
+];
